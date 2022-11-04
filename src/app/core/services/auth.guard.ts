1e58618779de1b104e717555d29cda630a2f1e12
Michał Pasynek <michalpasynek@gmail.com>
2022-11-04 09:03:24 +0100

    Fixing guard bug

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -1,27 +1,41 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.services';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
   constructor(
     private authService: AuthService,
     private router: Router,
     private toast: MatSnackBar
-    ){}
+  ) {}
+
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot):  boolean {
-    if( this.authService.isLoggedIn()) {
-      return true;
-    } 
-    this.router.navigate(['/login']);
-    this.toast.open('You are not authorized to see this page. Please log in!');
-    return false;
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.authState$.pipe(
+      map((state) => {
+        if (state !== null) {
+          return true;
+        }
+
+        this.router.navigate(['/login']);
+        this.toast.open(
+          'You are not authorized to see this page. Please log in'
+        );
+        return false;
+      })
+    );
   }
-  
 }
